b98f183352cd6edd15e14f4486b6e294686c96fb
javier <espindolajavier2013@gmail.com>
2024-03-09 01:03:31 -0300

    se agrego un icono de pagina par ala pestala, componentes: bussines , menu con sus respectivos css

--- src/components/Business.jsx
+++ src/components/Business.jsx
@@ -2,110 +2,101 @@ import { useState } from "react";
 import "../css/business.css";
 
 function Business() {
-  const [businessName, setBusinessName] = useState("");
-  const [products, setProducts] = useState([
-    { id: "", name: "", price: "", url: "" },
-  ]);
+  const [name, setBusinessName] = useState("");
+  const [id, setId] = useState("");
+  const [product, setProduct] = useState("");
+  const [price, setPrice] = useState("");
+  const [url, setUrl] = useState("");
 
-  // Función para manejar el cambio en el nombre del negocio
-  const handleBusinessNameChange = (e) => {
-    setBusinessName(e.target.value);
-  };
-
-  // Función para manejar el cambio en los productos
-  const handleProductChange = (e, index, field) => {
-    const newProducts = [...products];
-    newProducts[index][field] = e.target.value;
-    setProducts(newProducts);
-  };
-
-  // Función para agregar un nuevo producto
-  const addProduct = () => {
-    setProducts([...products, { id: "", name: "", price: "", url: "" }]);
-  };
-
-  // Función para realizar la solicitud POST
   const postBusiness = () => {
     fetch("http://localHost:8080/api/business", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ businessName, products }),
+      body: JSON.stringify({ name, products: [{ id, product, price, url }] }),
     })
       .then((response) => response.json())
       .then((data) => console.log("Data", data))
       .catch((error) => console.error("Error:", error));
   };
+  //hacer que actualice el arrar colocandole uno nuevo y enviarlo
 
   return (
     <>
       <div className="containerForm flexcolum ">
         <h2 className="businessTitle"> Restaurants</h2>
-        <div className="form ">
-          <label className="form__input">
-            <span className="form__input--span">Nombre Negocio</span>
+        <div className="form flexcolum ">
+          <label className="form__label">
             <input
+              required="required"
               className="form__input"
               type="text"
-              placeholder="Nombre del negocio"
-              value={businessName}
-              onChange={handleBusinessNameChange}
+              onChange={(e) => setBusinessName(e.target.value)}
             />
+            <span className="form__input--span">Nombre Negocio</span>
           </label>
-          {products.map((product, index) => (
-            <div className="flexcolum" key={index}>
-              <label className="productLabel">
-                <span className="form__input--span">ID producto</span>
-                <input
-                  className="form__input"
-                  type="text"
-                  placeholder={`ID del producto ${index + 1}`}
-                  value={product.id}
-                  onChange={(e) => handleProductChange(e, index, "id")}
-                />
-              </label>
 
-              <label className="productLabel">
-                <span className="form__input--span">Nombre</span>
-                <input
-                  className="form__input"
-                  type="text"
-                  placeholder={`Nombre del producto ${index + 1}`}
-                  value={product.name}
-                  onChange={(e) => handleProductChange(e, index, "name")}
-                />
-              </label>
+          <div className="product__form flexcolum">
+            <label className="productLabel">
+              <input
+                required="required"
+                className="form__input"
+                type="number"
+                onChange={(e) => setId(e.target.value)}
+              />
+              <span className="form__input--span">ID producto</span>
+            </label>
 
-              <label className="productLabel">
-                <span className="form__input--span">Precio</span>
-                <input
-                  className="form__input"
-                  type="number"
-                  placeholder={`Precio del producto ${index + 1}`}
-                  value={product.price}
-                  onChange={(e) => handleProductChange(e, index, "price")}
-                />
-              </label>
+            <label className="productLabel">
+              <input
+                required="required"
+                className="form__input"
+                type="text"
+                onChange={(e) => setProduct(e.target.value)}
+              />
+              <span className="form__input--span">Nombre</span>
+            </label>
 
-              <label className="productLabel">
-                <span className="form__input--span">URL imagen</span>
-                <input
-                  className="form__input"
-                  type="text"
-                  placeholder={`URL del producto ${index + 1}`}
-                  value={product.url}
-                  onChange={(e) => handleProductChange(e, index, "url")}
-                />
-              </label>
-            </div>
-          ))}
-          <div>
-            <button className="form__button" onClick={addProduct}>
-              Agregar otro producto
-            </button>
-            <button className="form__button" onClick={postBusiness}>
-              Enviar
-            </button>
+            <label className="productLabel">
+              <input
+                required="required"
+                className="form__input"
+                type="number"
+                onChange={(e) => setPrice(e.target.value)}
+              />
+              <span className="form__input--span">Precio</span>
+            </label>
+
+            <label className="productLabel">
+              <input
+                required="required"
+                className="form__input"
+                type="text"
+                onChange={(e) => setUrl(e.target.value)}
+              />
+              <span className="form__input--span">URL imagen</span>
+            </label>
           </div>
+          <div className="imgPrewiev">
+            {url && (
+              <img className="imgPrewiev" src={`${url}`} alt="imagen previa" />
+            )}
+          </div>
+        </div>
+
+        <div className="button__product">
+          <button
+            disabled={
+              name === "" ||
+              id === "" ||
+              product === "" ||
+              price === "" ||
+              url === ""
+            }
+            className="button__product--button"
+            onClick={postBusiness}
+          >
+            Enviar
+          </button>
         </div>
       </div>
     </>
